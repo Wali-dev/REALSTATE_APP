91e8973e3f0c2c1c07cb11f5c73d8ec2b08096d0
Wali-dev <cornelhassan780@gmail.com>
2026-01-17 02:03:10 +0530

    Initial Setup, configured nativewind, constants and assets

---
+++ app/(root)/properties/[id].tsx
@@ -0,0 +1,15 @@
+import { useLocalSearchParams } from 'expo-router';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Properties = () => {
+    const { id } = useLocalSearchParams();
+
+    return (
+        <View>
+            <Text>Properties: {id}</Text>
+        </View>
+    )
+}
+
+export default Properties
