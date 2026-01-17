5b048ad07244f10de5a3f18569ff7c99de70b170
Wali-dev <cornelhassan780@gmail.com>
2026-01-17 17:29:40 +0530

    appwrite configured

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,66 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { login } from "@/lib/appwrite";
+import React from "react";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context"; //Ensures content is within safe area boundaries
 
 const SignIn = () => {
-    return (
-        <View>
-            <Text>SignIn</Text>
-        </View>
-    )
-}
+  async function handleLogin() {
+    const result = await login();
 
-export default SignIn
+    if (result) {
+      console.log("Login Successfull");
+    } else {
+      Alert.alert("Failed to login");
+    }
+  }
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome to RealState
+          </Text>
+          <Text className="text-2xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer to {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            {" "}
+            Login to RealState with Google
+          </Text>
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md  shadow-zinc-300 rounded-full w-full  py-4 mt-5"
+          >
+            <View className="flex flex-row justify-start mx-auto gap-2 items-center">
+              <Image
+                source={icons.google}
+                className=" w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="font-rubik-medium text-black-300">
+                Continue With Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
